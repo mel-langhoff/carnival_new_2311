f6ede850ea616676220f12a5bfcb1ae22d2cf3c0
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-09 09:11:31 -0700

    Test: add test for #add_preference

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,10 +1,15 @@
 class Visitor
-    attr_reader :name, :height, :spending_money
+    attr_reader :name, :height, :spending_money, :preferences
 
     def initialize(name, height, spending_money)
         @name = name
         @height = height
         @spending_money = spending_money
+        @preferences = []
+    end
+
+    def add_preference(preference)
+        @preferences << preference
     end
 
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe Visitor do
         expect(@visitor1).to be_an_instance_of Visitor
     end
 
+    it "#add_preference)" do
+        @visitor1.add_preference(:gentle)
+        @visitor1.add_preference(:thrilling)
+
+        expect(@visitor1.preferences).to include(:thrilling)
+    end
+
 end
 

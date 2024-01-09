c87fc8e4c4ddfc431edd23a8515ed03969303033
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-09 10:15:07 -0700

    Finished iteration 2

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Ride do
     before :each do
         @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+        @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
         @visitor1 = Visitor.new("Bruce", 54, "$10")
         @visitor2 = Visitor.new("Tucker", 36, "$5")
         @visitor3 = Visitor.new("Penny", 64, "$15")

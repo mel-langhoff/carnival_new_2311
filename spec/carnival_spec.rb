a006f5593158667b6ee6bae6c47cc699dc2590de
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-09 10:21:53 -0700

    Test: add and pass test for exists

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Carnival do
+    before :each do
+        @carnival1 = Carnival.new
+    end
+
+    it "exists" do
+        expect(@carnival1).to be_an_instance_of Carnival
+    end
 
 end

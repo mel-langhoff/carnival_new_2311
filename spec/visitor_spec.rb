6c28e05cb6393f179c5e71eb1efdd94dbc8b10b8
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-09 09:08:38 -0700

    Initial commit

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,2 +1,13 @@
 require 'spec_helper'
 
+RSpec.describe Visitor do
+    before :each do
+        @visitor = Visitor.new(name, height, spending_money)
+    end
+
+    it "exists" do
+        expect(@visitor).to be_an_instance_of Visitor
+    end
+
+end
+

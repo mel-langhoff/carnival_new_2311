469ab0a162cd60ef32d11b8bb9e2f7645f8826c6
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-09 09:14:50 -0700

    Test: add test for #tall_enough?

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,17 +3,26 @@ require 'spec_helper'
 RSpec.describe Visitor do
     before :each do
         @visitor1 = Visitor.new("Bruce", 54, "$10")
+        @visitor2 = Visitor.new("Tucker", 36, "$5")
+        @visitor3 = Visitor.new("Penny", 64, "$15")
     end
 
     it "exists" do
         expect(@visitor1).to be_an_instance_of Visitor
     end
 
-    it "#add_preference)" do
+    it "#add_preference" do
         @visitor1.add_preference(:gentle)
         @visitor1.add_preference(:thrilling)
 
         expect(@visitor1.preferences).to include(:thrilling)
+    end
+
+    it "#tall_enough?" do
+        expect(@visitor1.tall_enough?(54)).to be true
+        expect(@visitor2.tall_enough?(54)).to be false
+        expect(@visitor3.tall_enough?(54)).to be true
+        expect(@visitor1.tall_enough?(64)).to be false
     end
 
 end
